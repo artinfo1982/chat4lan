3feae71f8f14193cea1949b319009698ec58e253
Artinfo <1269788142@qq.com>
2015-09-21 00:21:59 +0800

    Create Constant.py

---
+++ src/client/Constant.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# encoding=utf-8
+
+# login:
+# client login and server response success both use 0000 0001(\x01)
+LON_REQ_SUC_RSP = '\x01'
+# server response when client authentication failed, 1000 0011(\x83)
+SVR_RSP_LON_ERR_REP = '\x83'
+# register:
+# client register and server response success both use 0000 0010(\x02)
+REG_REQ_SUC_RSP = '\x02'
+# server response when client reach max user number, 1000 0001(\x81)
+SVR_RSP_REG_ERR_MAX_USR = '\x81'
+# server response when client use same name register twice, 1000 0010(\x82)
+SVR_RSP_REG_ERR_REP = '\x82'
