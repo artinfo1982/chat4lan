e268eb31117ebf41ce5312478d6a0bd7f0ae2c34
Artinfo <1269788142@qq.com>
2015-09-21 23:37:28 +0800

    Update Constant.py

--- src/client/Constant.py
+++ src/client/Constant.py
@@ -6,6 +6,9 @@
 LON_REQ_SUC_RSP = '\x01'
 # server response when client authentication failed, 1000 0011(\x83)
 SVR_RSP_LON_ERR_REP = '\x83'
+# server response when user is not in memory database, 1000 0100(\x84)
+SVR_RSP_LON_ERR_NOT_EXI = '\x84'
+
 # register:
 # client register and server response success both use 0000 0010(\x02)
 REG_REQ_SUC_RSP = '\x02'
