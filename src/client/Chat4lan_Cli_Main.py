c5aeee9f7ff17803cb0248931a7c149441190370
Artinfo <1269788142@qq.com>
2015-09-26 23:57:30 +0800

    Update Chat4lan_Cli_Main.py

--- src/client/Chat4lan_Cli_Main.py
+++ src/client/Chat4lan_Cli_Main.py
@@ -1,16 +1,6 @@
 #! /usr/bin/python
 # encoding=utf-8
 
-"""
-register:
-client --> (flag[1], username_len[1], password_len[1], username[?], password[?])
-server --> (flag[1], userid[1] or space[1])
-
-login:
-client --> (flag[1], password_len[1], userid[1], password[?])
-server --> (flag[1], list_size[1], list[?])
-"""
-
 import wx
 import Login
 
