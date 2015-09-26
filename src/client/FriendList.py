9ba7bfd4f968c96e7dbcfb051d3ebc5b7031ce2a
Artinfo <1269788142@qq.com>
2015-09-26 23:58:59 +0800

    Update FriendList.py

--- src/client/FriendList.py
+++ src/client/FriendList.py
@@ -1,10 +1,14 @@
 #! /usr/bin/python
-# -*- coding: utf-8 -*-
+# encoding=utf-8
 
 import wx
 import AddFriendDialog
 import Login
 import Constant
+import P2P_Chat
+
+P2P_ID = ""
+P2P_NAME = ""
 
 class FriendList(wx.Frame):
     def __init__(self, parent):
@@ -23,34 +27,48 @@ class FriendList(wx.Frame):
 
 
 class Panel_Single(wx.Panel):
+
+    lc = None
+
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        lc = wx.ListCtrl(self, size=(233, 410), style=wx.LC_REPORT)
-        lc.InsertColumn(0, u"用户名")
-        lc.InsertColumn(1, u"状态")
-        lc.SetColumnWidth(0, 140)
-        lc.SetColumnWidth(1, -2)
+        self.lc = wx.ListCtrl(self, size=(233, 410), style=wx.LC_REPORT)
+        self.lc.InsertColumn(0, u"ID")
+        self.lc.InsertColumn(1, u"用户名")
+        self.lc.InsertColumn(2, u"状态")
+        self.lc.SetColumnWidth(0, 50)
+        self.lc.SetColumnWidth(1, 140)
+        self.lc.SetColumnWidth(2, -2)
         for i in range(0, Login.FRIEND_NUM, 1):
-            lc.InsertStringItem(i, u'%s' % unicode(Login.FRIEND_NAME_ARRAY[i], 'utf-8'))
+            self.lc.InsertStringItem(i, u'%s' % unicode(Login.FRIEND_ID_ARRAY[i], 'utf-8'))
+            self.lc.SetStringItem(i, 1, u'%s' % unicode(Login.FRIEND_NAME_ARRAY[i], 'utf-8'))
             if Constant.FRI_ONLINE == Login.FRIEND_STATUS_ARRAY[i]:
-                lc.SetStringItem(i, 1, "ON")
+                self.lc.SetStringItem(i, 2, "ON")
             elif Constant.FRI_OFFLINE == Login.FRIEND_STATUS_ARRAY[i]:
-                lc.SetStringItem(i, 1, "OFF")
+                self.lc.SetStringItem(i, 2, "OFF")
 
         add_friend_button = wx.Button(self, label=u'添加好友', pos=(80, 420))
 
         self.Bind(wx.EVT_BUTTON, self.On_Add_Friend, add_friend_button)
-        self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.On_Chat_P2P)
+        self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.On_Chat_P2P)
         pass
 
     def On_Add_Friend(self, event):
         dialog = AddFriendDialog.AddFriendDialog(None)
         dialog.Centre()
         dialog.Show()
-        pass
 
     def On_Chat_P2P(self, event):
-        pass
+        global P2P_ID
+        global P2P_NAME
+        index = event.GetIndex()
+        item1 = self.lc.GetItem(index, 0)
+        item2 = self.lc.GetItem(index, 1)
+        P2P_ID = item1.GetText()
+        P2P_NAME = item2.GetText()
+        dialog = P2P_Chat.P2P_Chat(None)
+        dialog.Centre()
+        dialog.Show()
 
 
 class Panel_Multi(wx.Panel):
