1c5d4e6c65448b4f2d0b93f901f94f416f3b292c
Artinfo <1269788142@qq.com>
2015-09-29 23:47:18 +0800

    Update Chat4lan_Cli_Main.py

--- src/client/Chat4lan_Cli_Main.py
+++ src/client/Chat4lan_Cli_Main.py
@@ -2,11 +2,23 @@
 # encoding=utf-8
 
 import wx
+import threading
 import Login
+import MsgRecver
+
+class GUI(threading.Thread):
+    def run(self):
+        app = wx.App()
+        login = Login.Login(None)
+        login.Centre()
+        login.Show()
+        app.MainLoop()
+
+def main():
+    t_gui = GUI()
+    t_gui.start()
+    t_msgRecver = MsgRecver.MsgRecv()
+    t_msgRecver.start()
 
 if __name__ == '__main__':
-    app = wx.App()
-    login = Login.Login(None)
-    login.Centre()
-    login.Show()
-    app.MainLoop()
+    main()
