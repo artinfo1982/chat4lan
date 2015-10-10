d8a1eb4eb93f468ea64b46470009fc62deb613a7
Artinfo <1269788142@qq.com>
2015-10-10 22:01:03 +0800

    Update and rename P2P_Chat.py to P2P_Group_Chat.py

--- src/client/P2P_Group_Chat.py
+++ src/client/P2P_Group_Chat.py
@@ -3,15 +3,65 @@
 
 import wx
 import socket
+import threading
+import time
 import FriendList
 import Constant
 import Login
 import Dialog
+import Tools
 
 MSG_RECV = None
+LOCAL_IP = ""
+
+class MsgRecv(threading.Thread):
+
+    def run(self):
+
+        global SENDER_ID
+        global SENDER_MSG
+        global LOCAL_IP
+        global MSG_RECV
+
+        LOCAL_IP = Tools.get_local_ip()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        while True:
+            if not Login.isParamOK:
+                time.sleep(1)
+            else:
+                break
+
+        sock.bind((LOCAL_IP, int(Login.LOCAL_PORT)))
+        sock.listen(64)
+
+        while True:
+            connection, address = sock.accept()
+            try:
+                connection.settimeout(10)
+                buf = connection.recv(1024)
+                if Constant.SEND_RECV_REQ_SUC_RSP == buf[0]:
+                    senderID_len = ord(buf[1])
+                    msg_len = ord(buf[2])
+                    index = 3
+                    SENDER_ID = buf[index:(index + senderID_len)]
+                    index += senderID_len
+                    SENDER_MSG = buf[index:(index + msg_len)]
+                    MSG_RECV.AppendText(FriendList.P2P_NAME + "  "
+                                        + time.strftime('%Y-%m-%d %X', time.localtime(time.time())) + "\n"
+                                        + str(SENDER_MSG).decode('utf-8') + '\n')
+                else:
+                    connection.close()
+                    continue
+            except Exception, e:
+                continue
+            finally:
+                connection.close()
+
 
 class P2P_Chat(wx.Frame):
 
+    msg_recv = None
     msg_send = None
     isSuccess = True
 
@@ -29,7 +79,7 @@ class P2P_Chat(wx.Frame):
                                 | wx.CLOSE_BOX
                                 | wx.CLIP_CHILDREN)
         p2p_panel = wx.Panel(self)
-        MSG_RECV = wx.TextCtrl(p2p_panel, size=(450, 300), style=wx.TE_READONLY)
+        MSG_RECV = wx.TextCtrl(p2p_panel, size=(450, 300), style=wx.TE_READONLY | wx.TE_MULTILINE)
         MSG_RECV.SetBackgroundColour(wx.Colour(192, 220, 192))
         self.msg_send = wx.TextCtrl(p2p_panel, size=(450, 100), style=wx.TE_PROCESS_ENTER)
         self.msg_send.SetBackgroundColour(wx.Colour(192, 220, 192))
@@ -49,6 +99,9 @@ class P2P_Chat(wx.Frame):
         pass
 
     def On_Send_Clear(self, event):
+
+        global MSG_RECV
+
         rsp = None
         msg = self.msg_send.GetValue().encode('utf-8')
         send_data = Constant.SEND_RECV_REQ_SUC_RSP + \
@@ -74,6 +127,9 @@ class P2P_Chat(wx.Frame):
         if self.isSuccess:
             if Constant.SEND_RECV_REQ_SUC_RSP == rsp[0]:
                 self.msg_send.Clear()
+                MSG_RECV.AppendText(Login.USERNAME + "  "
+                                    + time.strftime('%Y-%m-%d %X', time.localtime(time.time())) + "\n"
+                                    + str(msg).decode('utf-8') + "\n")
             else:
                 dialog = Dialog.Dialog(None, u"错误", u"消息发送失败", 200, 150, 30, 60)
                 dialog.Centre()
