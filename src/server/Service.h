5cd104adc7278e02549fa06756eab53ed1b29834
Artinfo <1269788142@qq.com>
2015-11-29 22:11:20 +0800

    Create Service.h

---
+++ src/server/Service.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "Constants.h"
+
+int 
+loginHandler 
+(FILE * fp, char * dbIP, char * dbUserName, char * dbPass, char * dbName, int dbPort, int sock, char * buffer, char * data);
+
+int 
+registerHandler 
+(FILE * fp, char * dbIP, char * dbUserName, char * dbPass, char * dbName, int dbPort, int sock, char * buffer, char * data);
+
+int 
+addFriendHandler 
+(FILE * fp, char * dbIP, char * dbUserName, char * dbPass, char * dbName, int dbPort, int sock, char * buffer, char * data);
+
+int 
+p2pChatHandler 
+(FILE * fp, char * dbIP, char * dbUserName, char * dbPass, char * dbName, int dbPort, int sock, char * buffer, char * data);
+
+int 
+addGroupHandler 
+(FILE * fp, char * dbIP, char * dbUserName, char * dbPass, char * dbName, int dbPort, int sock, char * buffer, char * data);
+
+int 
+groupChatHandler 
+(FILE * fp, char * dbIP, char * dbUserName, char * dbPass, char * dbName, int dbPort, int sock, char * buffer, char * data);
