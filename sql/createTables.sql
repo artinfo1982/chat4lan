7176d159829256a074000cbfd14c27e439342239
Artinfo <1269788142@qq.com>
2015-12-07 23:39:26 +0800

    Update createTables.sql

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -14,7 +14,7 @@ CREATE  TABLE IF NOT EXISTS `chatDB`.`usr_info` (
   `usr_password` VARCHAR(64) BINARY NOT NULL ,
   `usr_friend_num` INT UNSIGNED NOT NULL ,
   `usr_group_num` INT UNSIGNED NOT NULL ,
-  `usr_status` INT NOT NULL ,
+  `usr_status` VARCHAR(1) NOT NULL ,
   `usr_local_ip` VARCHAR(16) NOT NULL ,
   `usr_local_port` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`usr_id`) ,
@@ -62,22 +62,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `chatDB`.`group_member_info`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `chatDB`.`group_member_info` (
-  `index` INT NOT NULL AUTO_INCREMENT ,
-  `group_id` INT UNSIGNED NOT NULL ,
-  `member_id` INT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`index`) ,
-  UNIQUE INDEX `index_UNIQUE` (`index` ASC) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `chatDB`.`usr_resend_info`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `chatDB`.`usr_resend_info` (
   `index` INT NOT NULL AUTO_INCREMENT ,
+  `msg_recv_time` DATETIME NOT NULL ,
   `usr_id` INT UNSIGNED NOT NULL ,
   `usr_friend_id` INT UNSIGNED NOT NULL ,
   `usr_resend_msg` VARCHAR(1024) BINARY NOT NULL ,
