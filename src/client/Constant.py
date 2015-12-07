1f714824265ae94df9c8a0e8c98ac53eae99d899
Artinfo <1269788142@qq.com>
2015-12-07 23:35:32 +0800

    Update Constant.py

--- src/client/Constant.py
+++ src/client/Constant.py
@@ -1,17 +1,21 @@
-#! /usr/bin/python
 # encoding=utf-8
 
 # friend status:
-FRI_ONLINE = '\xF0'
-FRI_OFFLINE = '\xFF'
+FRI_ONLINE = '1'
+FRI_OFFLINE = '0'
+
+# mysql db is not reachable
+SVR_MYSQL_UNREACHABLE = '\x80'
 
 # login:
 # client login and server response success both use 0000 0001(\x01)
-LON_REQ_SUC_RSP = '\x01'
+LON_IN_REQ_SUC_RSP = '\x01'
 # server response when client authentication failed, 1000 0011(\x83)
 SVR_RSP_LON_ERR_REP = '\x83'
 # server response when user is not in memory database, 1000 0100(\x84)
 SVR_RSP_LON_ERR_NOT_EXI = '\x84'
+# server response when user is already login, 1000 1010(\x8A)
+SVR_RSP_USR_LOG_IN_REP = '\x8A'
 
 # register:
 # client register and server response success both use 0000 0010(\x02)
@@ -38,5 +42,26 @@ SVR_SEND_MSG_FAIL_P2P = '\x87'
 # add group:
 # client add group and server response success both use 0000 0101(\x05)
 ADD_GRO_REQ_SUC_RSP = '\x05'
-# server response when client add group but already exist, 1000 1000(\x88)
-SVR_RSP_ADD_GRO_ERR_EXI = '\x88'
+# server response when client add group reach max group number, 1000 1000(\x88)
+SVR_RSP_REG_ERR_MAX_GRO = '\x88'
+# server response when client add group but already exist, 1000 1001(\x89)
+SVR_RSP_ADD_GRO_ERR_EXI = '\x89'
+
+# send and recv msg group:
+# group client send msg and server send to other success both use 0000 0110(\x06)
+SEND_RECV_REQ_SUC_RSP_GRO = '\x06'
+
+# logout:
+# client logout and server response success both use 0000 0111(\x07)
+LON_OUT_REQ_SUC_RSP = '\x07'
+# server response when user is already logout, 1000 1011(\x8B)
+SVR_RSP_USR_LOG_OUT_REP = '\x8B'
+
+# query group member list request:
+# client query group member list and server response success both use 0000 1000(\x08)
+GRO_MEM_LST_REQ_SUC_RSP = '\x08'
+# query group member list error, 1000 1100(\x8C)
+GRO_MEM_LST_REQ_ERR_RSP = '\x8C'
+
+# heart between client and server, use 0000 1001(\x09)
+CLI_SVR_HEART_BEAT = '\x09'
