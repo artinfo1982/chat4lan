dbb30ccdc1f604368d3d4504675b4f4313751841
Artinfo <1269788142@qq.com>
2015-12-13 23:26:00 +0800

    Create Main.py

---
+++ src/client/Main.py
@@ -0,0 +1,24 @@
+# encoding=utf-8
+
+import wx
+import threading
+import Login
+import P2P_Group_Chat
+
+class GUI(threading.Thread):
+    def run(self):
+        app = wx.App()
+        login = Login.Login(None)
+        login.SetMaxSize((300, 250))
+        login.Centre()
+        login.Show()
+        app.MainLoop()
+
+def main():
+    t_gui = GUI()
+    t_gui.start()
+    t_msgRecver = P2P_Group_Chat.MsgRecv()
+    t_msgRecver.start()
+
+if __name__ == '__main__':
+    main()
