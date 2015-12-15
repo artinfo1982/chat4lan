e95aba10d70c369510fb332310776b1b2167890f
Artinfo <1269788142@qq.com>
2015-12-15 22:36:09 +0800

    Update testDataInit.sql

--- sql/testDataInit.sql
+++ sql/testDataInit.sql
@@ -1,9 +1,9 @@
 insert into usr_info(usr_name, usr_password, usr_friend_num, usr_group_num, usr_status, usr_local_ip, usr_local_port) 
-values ('a', 'a', 2, 1, '1', '192.168.3.12', 60000);
+values ('张三', 'a', 2, 1, '0', '', 0);
 insert into usr_info(usr_name, usr_password, usr_friend_num, usr_group_num, usr_status, usr_local_ip, usr_local_port) 
-values ('b', 'b', 1, 1, '1', '192.168.3.12', 50000);
+values ('李四', 'b', 1, 1, '0', '', 0);
 insert into usr_info(usr_name, usr_password, usr_friend_num, usr_group_num, usr_status, usr_local_ip, usr_local_port) 
-values ('c', 'c', 1, 1, '1', '192.168.3.12', 40000);
+values ('王五', 'c', 1, 1, '0', '', 0);
 
 insert into usr_friend_info(usr_id, usr_friend_id) 
 values (1, 2);
@@ -22,4 +22,4 @@ insert into usr_group_info(usr_id, group_id)
 values (3, 1);
 
 insert into group_info(group_name, group_member_num, group_online_member_num) 
-values ('A', 3, 3);
+values ('桌球群', 3, 0);
